4ec941335998352a92bbae28e43715070ae4b8b1
Vincent <vincent.ogloblinsky@gmail.com>
2014-12-17 07:18:10 +0100

    Server : camera live streaming proxy with thumbnail

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,28 @@
 'use strict';
 
+var request = require('request').defaults({ encoding: null }),
+    fs = require('fs-extended');
+
 module.exports = function(app) {
-    
+
     app.route('/api/life')
         .get(function(req, res, next) {
             res.send(true);
+        });
+
+    app.route('/api/camera/:id/thumb')
+        .get(function(req, res, next) {
+            var _cameraId = req.params.id,
+                _fileName = '/tmp/cam-thumb' + _cameraId + '.jpg',
+                _contentType = null,
+                _finalURI = null;
+
+            request('http://192.168.42.1/mjpeg/amba.jpg', function (error, response, body) {
+                _contentType = response.headers['content-type'];
+                _finalURI = 'data:' + _contentType + ';base64,' + new Buffer(body).toString('base64');
+                res.send(_finalURI);
+            });
+           
         });
 
 };
