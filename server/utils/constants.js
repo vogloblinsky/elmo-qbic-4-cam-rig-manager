0099301fc465cfea3056d01aa98824acede6616c
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2015-01-12 07:31:19 +0100

    Server constants module

---
+++ server/utils/constants.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var constants = {
+    localIPCamera: '192.168.42.1',
+    namespaceCameraName: 'QBIC-VOG-',
+    defaultCameraPassword: '0123456789'
+};
+
+module.exports = constants;
